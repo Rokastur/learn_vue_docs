e1d1f8e6e388745a025fbc9e599a898dc2532da2
Rokas <rokasatminas@gmail.com>
2025-08-10 20:37:18 +0300

    reformat, function attempt

--- vue-learn/src/App.vue
+++ vue-learn/src/App.vue
@@ -1,38 +1,45 @@
 <script setup lang="ts">
-  import type { ButtonHTMLAttributes } from 'vue'
+import { type ButtonHTMLAttributes, ref } from 'vue'
 
-  const message = "message is here"
-  const additionalMessage = "extra information"
+const message = 'message is here'
+const additionalMessage = 'extra information'
 
-  const buttonProps: ButtonHTMLAttributes = {
-    id: 'submitBtn',
-    type: 'submit',
-    disabled: false
-  };
+const buttonProps: ButtonHTMLAttributes = {
+  id: 'submitBtn',
+  type: 'submit',
+  disabled: false,
+}
 
-  const isButtonDisabled = true;
+const isButtonDisabled = false
+
+//   reactivity:
+//recommended way is to use the ref() function.
+//ref wraps the argument into an object and provides .value property.
+const count = ref(0)
+count.value++
+const countFunction = ref(0)
+
+function increment() {
+  countFunction.value++
+}
 </script>
 
-
 <template>
-
   <h1>You did it!</h1>
   <p>Hi</p>
   <p>Please enter the message</p>
-  <p> {{message}} + {{additionalMessage}}</p>
-
-
-
+  <p>{{ message }} + {{ additionalMessage }}</p>
 
   <!-- Take JS var dynamicId and set it as the value of the HTML attribute id for this div
        If dynamicId changes in the component, Vue will automatically update the id in the DOM. -->
-  <button v-bind="buttonProps">Submit</button>
+  <button v-bind="buttonProps" @click="count++">Submit</button>
 
-  <br><br><br>
-  <button :disabled="isButtonDisabled">Button</button>
-  <br><br><br>
-
-
+  <br /><br /><br />
+  <button :disabled="isButtonDisabled" @click="count--">Button</button>
+  <br /><br /><br />
+  <p>Current count value: {{ count }}</p>
+  <br /><br /><br />
+  <button @click="increment">Function button {{ countFunction }}</button>
 </template>
 
 <style scoped></style>
