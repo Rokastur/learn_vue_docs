eedc454719f8acb03850976e4ff1d0d358d1faba
Rokas <rokasatminas@gmail.com>
2025-08-10 14:35:04 +0300

    mustache syntax text output

--- vue-learn/src/App.vue
+++ vue-learn/src/App.vue
@@ -1,8 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const message = "message is here"
+  const additionalMessage = "extra information"
+</script>
+
 
 <template>
   <h1>You did it!</h1>
   <p>Hi</p>
+  <p>Please enter the message</p>
+  <p> {{message}} + {{additionalMessage}}</p>
 </template>
 
 <style scoped></style>
