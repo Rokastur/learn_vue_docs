8ab48da8a084306dde3c166e0e9efc47aa9b31ae
Rokas <rokasatminas@gmail.com>
2025-08-12 23:19:13 +0300

    move previous items to another file, create and import child component

--- vue-learn/src/App.vue
+++ vue-learn/src/App.vue
@@ -1,32 +1,10 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
-
-const question = ref('')
-const answer = ref('Questions usually contain a question mark. ;-)')
-const loading = ref(false)
-
-watch(question, async (newQuestion, oldQuestion) => {
-  if (newQuestion.includes('?')) {
-    loading.value = true
-    answer.value = 'Thinking...'
-    try {
-      const res = await fetch('https://yesno.wtf/api')
-      answer.value = (await res.json()).answer
-    } catch (error) {
-      answer.value = 'Error! Could not reach the API. ' + error
-    } finally {
-      loading.value = false
-    }
-  }
-})
+import ButtonCounter from './ButtonCounter.vue'
 </script>
 
 <template>
-  <p>
-    Ask a yes/no question:
-    <input v-model="question" :disabled="loading" />
-  </p>
-  <p>{{ answer }}</p>
+  <h1>Here is a child component!</h1>
+  <ButtonCounter />
 </template>
 
 <style scoped></style>
