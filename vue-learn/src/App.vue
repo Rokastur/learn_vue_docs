70bb1583e05280abddb366ae67e27847d751ec09
Rokas <rokasatminas@gmail.com>
2025-08-13 21:59:15 +0300

    component basics and tabs

--- vue-learn/src/App.vue
+++ vue-learn/src/App.vue
@@ -1,10 +1,45 @@
 <script setup lang="ts">
 import ButtonCounter from './ButtonCounter.vue'
+import BlogPost from '@/BlogPost.vue'
+import Home from './Home.vue'
+import Post from './Post.vue'
+import Archive from './Archive.vue'
+import { ref } from 'vue'
+
+const postFontSize = ref(1)
+
+const currentTab = ref('Home')
+const tabs = {
+  Home,
+  Post,
+  Archive,
+}
 </script>
 
 <template>
-  <h1>Here is a child component!</h1>
-  <ButtonCounter />
+  <div :style="{ fontSize: postFontSize + 'em' }">
+    <h1>Here is a child component!</h1>
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
+    <BlogPost title="My journey with Vue" />
+    <BlogPost title="Blogging with Vue" @enlarge-text="postFontSize += 0.3" />
+    <BlogPost title="Why Vue is so fun" @enlarge-text="postFontSize += 0.1" />
+
+    <div>
+      <button
+        v-for="(_, tab) in tabs"
+        :key="tab"
+        @click="currentTab = tab"
+      >
+        {{tab}}
+      </button>
+      <component :is="tabs[currentTab]"></component>
+
+    </div>
+
+  </div>
+
 </template>
 
 <style scoped></style>
---
+++ vue-learn/src/BlogPost.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+defineProps(['title'])
+
+</script>
+
+<template>
+  <h4>{{ title }}</h4>
+  <button @click="$emit('enlarge-text')">Enlarge text</button>
+</template>
+
+<style scoped></style>
